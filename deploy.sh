d75180cd9fb7dc7a948d44b5aa4295f39bfc1fed
AdrianLeonSunyol <aals1@alu.ua.es>
2020-05-01 10:58:18 -0400

    added ingress nginx

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
-cd ./odoo && kubectl apply -f web-deployment.yaml,web-service.yaml
+cd ./ingress && kubectl apply -f nginx-ingress.yml
+cd ../odoo && kubectl apply -f web-deployment.yaml,web-service.yaml
 cd ../postgres/ && kubectl apply -f db-data-persistentvolumeclaim.yaml,db-deployment.yaml,db-service.yaml,web-claim1-persistentvolumeclaim.yaml,web-claim2-persistentvolumeclaim.yaml,web-data-persistentvolumeclaim.yaml
